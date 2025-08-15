e4a16dc9387b22e9f94e4ddc08291d5eaf6ff445
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2025-08-15 12:22:59 +0100

    setup icons

---
+++ src/lib/my-components/logo.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+	let { 
+        name = "default",
+        href = undefined,
+        new_window = false,
+        w = 10,
+        h = 10
+    } = $props();
+</script>
+
+<Button
+    variant="outline"
+    class={`w-${w} h-${h} p-1`}
+    href={href}
+    target={new_window ? "_blank" : undefined}
+    rel={new_window ? "noopener noreferrer" : undefined}
+>
+    <img src={`/icons/${name}.svg`} alt={name} />
+</Button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-home
+<script lang="ts">
+	import Logo from "$lib/my-components/logo.svelte";
+</script>
+
+<Logo name="orcid" href="https://orcid.org/0009-0006-5669-2378" new_window/>
+
+Home
